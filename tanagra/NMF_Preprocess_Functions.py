1c39dcf529cf846aa530ce02f6c7f15408cd6ba1
Bill Konyk <38260957+konykwj@users.noreply.github.com>
2022-12-13 05:29:56 -0600

    Updating preprocessor with new vectorization method

--- tanagra/NMF_Preprocess_Functions.py
+++ tanagra/NMF_Preprocess_Functions.py
@@ -10,9 +10,13 @@ import re
 from nltk import word_tokenize, pos_tag
 from nltk.stem import WordNetLemmatizer 
 from nltk.corpus import wordnet, stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import gensim
 from gensim.models.phrases import Phrases, Phraser
+import numpy as np
+from scipy.sparse import spdiags
+from sklearn.preprocessing import normalize
+
 
 def get_wordnet_pos(treebank_tag):
     """
@@ -138,7 +142,7 @@ def create_corpus(text_df, stopword_list, text_key):
         
     return corpus
 
-def identify_bigrams(corpus, gensim_min_count, gensim_threshold):
+def identify_bigrams(corpus, gensim_min_count, gensim_threshold, gensim_sentences = []):
     """
     Identifies bigrams in the text
     
@@ -159,7 +163,8 @@ def identify_bigrams(corpus, gensim_min_count, gensim_threshold):
         A tokenized version of the corpus for use in creating the word-2-vec model
     """
     
-    gensim_sentences = create_gensim_sentences(corpus)
+    if len(gensim_sentences) == 0:
+        gensim_sentences = create_gensim_sentences(corpus)
         
     bigram = Phraser(Phrases(gensim_sentences, min_count = gensim_min_count, threshold = gensim_threshold))
         
@@ -215,7 +220,9 @@ def create_w2v_skipgram_model(gensim_sentences, w2v_location, min_word_count):
 
     w2v_model.save(w2v_location)
         
-def vectorize_corpus(corpus, vectorized_topic_location, min_word_count):
+def vectorize_corpus(corpus, vectorized_topic_location, min_word_count, 
+                     vec_type = 'tfidf',
+                     log_base = 2):
     """
     Vectorize the corpus using the TF-IDF scheme
     
@@ -227,16 +234,89 @@ def vectorize_corpus(corpus, vectorized_topic_location, min_word_count):
         File and path for where to save the vectorized model
     min_word_count: int
         Minimum numbe of times that words must appear to be counted
+    vec_type: str
+        Type of vectorization to use. Options are "info" or "tfidf"
+    log_base: int
+        Base of log scaling to use. Defaults to 2
+  
         
     Returns
     -------
     None
     """
-
-    tfidf_vect = TfidfVectorizer(min_df=min_word_count) # Creates the vectorizer instance
     
-    word_tfidf = tfidf_vect.fit_transform(corpus) #vectorizes the corpus
-    tfidf_word_list = tfidf_vect.get_feature_names() #Gets the feature names
-    pickle.dump([word_tfidf,tfidf_word_list], open( vectorized_topic_location, "wb" ) ) #Saves the file.
+    
+    if vec_type == 'info':
+        
+        cv = CountVectorizer(analyzer='word', ngram_range=(1, 1), min_df = min_word_count)
+
+        doc_term_mat = cv.fit_transform(corpus)
+                
+        #Converting to binary; whether a word appears in the corpus or not
+        count_mat = doc_term_mat.copy()
+        count_mat[count_mat>0] = 1
+
+        #Creates the number of times that each word occurrs in a document with another word
+        co_occurrence_mat = count_mat.T.dot(count_mat)
+
+        #Converting to the probability of finding a particular pair of words in a document
+        co_occurrence_mat.data =  co_occurrence_mat.data / co_occurrence_mat.sum() 
+
+        #The diagonal represents the probability of finding a given word in a document
+        p_word = co_occurrence_mat.diagonal()
+        p_word = p_word / p_word.sum()
+
+        #Creating matrix for mutual information calculation
+        # p_mat_inv = csc_array(np.diag(1/p_word))
+        
+        p_mat_inv = spdiags(1/p_word, 0, p_word.size, p_word.size)
+        p_mat = p_mat_inv.copy()
+        p_mat.data = 1/p_mat.data
+
+        #converting the co_occurrence_mat into a joint probability distribution and calculating informaiton with it
+        co_occurrence_mat.setdiag(0) #Removing the diagonal count from the calculation
+
+        #Creating the base matrix for calculating mutual information
+        mutual_i_mat = co_occurrence_mat.dot(p_mat_inv)
+        mutual_i_mat = p_mat_inv.dot(mutual_i_mat)
+
+        mutual_i_mat.data = np.log2(mutual_i_mat.data)/np.log2(log_base)
+
+        #With the distributions defined some information can be negative.
+        #These are set to zero.
+        mutual_i_mat.data[mutual_i_mat.data <0] = 0
+
+        #With this we have a series of weights for each term and pair of terms
+        #Modifying p_mat to refer to information (without making a new copy)
+        p_mat.data = -np.log2(p_mat.data)/np.log2(log_base)
+
+        # #Adding in 'rarity' weight to doc-term mat
+        doc_term_mat = doc_term_mat.dot(p_mat)
+
+        # #Multiplying this by the mutual information to get the context sum per document
+        context_mat = count_mat.dot(mutual_i_mat)
+
+        # #Using element-wise multiplication to merge the two matrices
+        doc_term_mat = doc_term_mat.multiply(context_mat)
+
+        #Normalizing so that the L2 norm is one
+        doc_term_mat = normalize(doc_term_mat, 'l2', axis = 1 )
+
+        vocab = cv.get_feature_names_out()
+                
+        pickle.dump([doc_term_mat, vocab], open( vectorized_topic_location, "wb" ) ) #Saves the file.
+        
+    
+    else: 
+        tfidf_vect = TfidfVectorizer(min_df = min_word_count) # Creates the vectorizer instance
+        
+        word_tfidf = tfidf_vect.fit_transform(corpus) #vectorizes the corpus
+        tfidf_word_list = tfidf_vect.get_feature_names() #Gets the feature names
+        pickle.dump([word_tfidf, tfidf_word_list], open( vectorized_topic_location, "wb" ) ) #Saves the file.
+    
+    
+    
+    
+    
     
 
